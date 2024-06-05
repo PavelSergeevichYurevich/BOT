4f637e7d1fb92fa5cfcd34bd153fe560c01c14dc
PavelSergeevichYurevich <Pasha2028@icloud.com>
2024-06-05 17:06:05 +0700

    start add token ops

--- backend/routes.py
+++ backend/routes.py
@@ -1,9 +1,15 @@
+from datetime import datetime, timedelta, timezone
 import os
-from fastapi import Request, Response, status
+from typing import Annotated
+from fastapi import HTTPException, Request, Response, status, Depends
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi.responses import FileResponse
 from pytube import Search, YouTube
 import requests
+import jwt
+from jwt.exceptions import InvalidTokenError
 from fastapi import APIRouter
+from backend.token_classes import TokenData
 from classes import Connect
 from schemas import UserCheckSchema, UserCreateSchema, UserSearchSchema, TrackOutSchema
 from fastapi.responses import HTMLResponse, RedirectResponse
@@ -15,6 +21,7 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 10080
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 music_parser_router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
@@ -23,6 +30,37 @@ def verify_password(plain_password, hashed_password):
 
 def get_password_hash(password):
     return pwd_context.hash(password)
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    
+    connect = Connect('database.db')
+    user = connect.get_user_username(username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
 
 @music_parser_router.get('/')
 def index(request: Request):
